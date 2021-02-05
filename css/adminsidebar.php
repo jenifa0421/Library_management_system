55c359cdcc5fec1b1a9ee4a7173a8633f2f4bd70
jenifa0421 <55661029+jenifa0421@users.noreply.github.com>
2021-02-05 11:06:48 +0530

    Add files via upload

---
+++ css/adminsidebar.php
@@ -0,0 +1,10 @@
+<ul>
+<li><a href="ahome.php"> Home</a><li>
+<li><a href="viewstudent.php"> View Student</a><li>
+<li><a href="uploadbook.php"> Upload Books </a><li>
+<li><a href="viewbook.php">View Books</a><li>
+<li><a href="viewrequest.php">View Request</a><li>
+<li><a href="viewcomments.php">View Comments</a><li>
+<li><a href="achangepassword.php"> Change Password</a><li>
+<li><a href="logout.php">Logout</a><li>
+</ul>
---
+++ css/usersidebar.php
@@ -0,0 +1,8 @@
+<ul>
+<li><a href="uhome.php">Home</a><li>
+<li><a href="searchbooks.php">Search Books</a><li>
+<li><a href="request.php">Request</a><li>
+<li><a href="uchangepassword.php">Change Password</a><li>
+<li><a href="logout.php">Logout</a><li>
+
+</ul>
